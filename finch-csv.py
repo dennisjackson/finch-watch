7698d5561f777ff8f2730aced88bb86eaf7abe7d
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2023-09-19 12:28:28 +0100

    Filter rows

--- finch-csv.py
+++ finch-csv.py
@@ -39,7 +39,9 @@ with open(output_file, 'w', newline='') as csv_file:
                 disable_feature = ", ".join(feature_association.get("disableFeature", []))
                 forcing_feature_on = feature_association.get("forcingFeatureOn", "")
                 forcing_feature_off = feature_association.get("forcingFeatureOff", "")
-
-            writer.writerow([name, consistency, exp_name, probability_weight, enable_feature, disable_feature, forcing_feature_on, forcing_feature_off, min_version, channel, platform, end_date])
+            if probability_weight in ["0", "", 0]:
+                continue
+            else:
+                writer.writerow([name, consistency, exp_name, probability_weight, enable_feature, disable_feature, forcing_feature_on, forcing_feature_off, min_version, channel, platform, end_date])
 
 print(f"CSV data has been written to {output_file}")
